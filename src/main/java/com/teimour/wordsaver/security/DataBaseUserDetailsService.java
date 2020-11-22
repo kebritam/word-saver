1e9d6eb77fdbca3adfd4e6373c329cbde3f60f99
Amirali Tavakoli <amiralitavakoli80@gmail.com>
2020-11-22 22:49:19 +0330

    import data from database

--- src/main/java/com/teimour/wordsaver/security/DataBaseUserDetailsService.java
+++ src/main/java/com/teimour/wordsaver/security/DataBaseUserDetailsService.java
@@ -1,8 +1,12 @@
 package com.teimour.wordsaver.security;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.sql.*;
 
 /**
  * @author kebritam
@@ -14,6 +18,21 @@ public class DataBaseUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        String password="";
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/word_saver",
+                    "postgres", "48625");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users WHERE username='"+username+"';");
+            resultSet.next();
+            password=resultSet.getObject("password", String.class);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return User.builder()
+                .username(username)
+                .password(password)
+                .build();
     }
 }
