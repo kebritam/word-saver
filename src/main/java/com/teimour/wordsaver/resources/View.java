06b733c2aea10cd39d890c45352502a2bdeb29db
Amirali Tavakoli <amiralitavakoli80@gmail.com>
2020-12-08 12:27:47 +0330

    move html resources to directories

--- src/main/java/com/teimour/wordsaver/resources/View.java
+++ src/main/java/com/teimour/wordsaver/resources/View.java
@@ -7,17 +7,22 @@ package com.teimour.wordsaver.resources;
  */
 
 public class View {
+    private static final String CATEGORY_DIR = "category/";
+    private static final String DEFINITION_DIR = "definition/";
+    private static final String EXCEPTION_DIR = "exceptions/";
+    private static final String FIRST_PAGE_DIR = "first-page/";
+    private static final String WORD_DIR = "word/";
 
-    public static final String INDEX="index";
-    public static final String LOGIN="login";
-    public static final String WORD="word";
-    public static final String WORD_FORM="wordForm";
-    public static final String NEW_WORD_FORM="newWordForm";
-    public static final String HOME="home";
-    public static final String DEFINITION_FORM="definitionForm";
-    public static final String NEW_DEFINITION_FORM="newDefinitionForm";
-    public static final String CATEGORY_FORM="categoryForm";
-    public static final String NEW_CATEGORY_FORM="newCategoryForm";
-    public static final String CATEGORY="category";
-    public static final String NOT_FOUND="404Error";
+    public static final String INDEX = FIRST_PAGE_DIR + "index";
+    public static final String LOGIN = FIRST_PAGE_DIR + "login";
+    public static final String WORD = WORD_DIR + "word";
+    public static final String WORD_FORM = WORD_DIR + "wordForm";
+    public static final String NEW_WORD_FORM = WORD_DIR + "newWordForm";
+    public static final String HOME = WORD_DIR + "home";
+    public static final String DEFINITION_FORM = DEFINITION_DIR + "definitionForm";
+    public static final String NEW_DEFINITION_FORM = DEFINITION_DIR + "newDefinitionForm";
+    public static final String CATEGORY_FORM = CATEGORY_DIR + "categoryForm";
+    public static final String NEW_CATEGORY_FORM = CATEGORY_DIR + "newCategoryForm";
+    public static final String CATEGORY = CATEGORY_DIR + "category";
+    public static final String NOT_FOUND = EXCEPTION_DIR + "404Error";
 }
